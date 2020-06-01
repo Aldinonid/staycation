366b384e8b1bfdcc066a9e00cffb735ef5a63e74
aldinonid <aldino.efendi7@gmail.com>
2020-06-01 19:01:27 +0700

    Finish add elements/Stepper and add some Form inputText & File

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "assets/scss/style.scss";
 
 import LandingPage from "pages/LandingPage";
-import Example from "pages/Example";
 import DetailsPage from "pages/DetailsPage";
+import CheckoutPage from "pages/CheckoutPage";
+import Example from "pages/Example";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Router>
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/properties/:id" component={DetailsPage} />
+        <Route path="/checkout" component={CheckoutPage} />
         <Route path="/example" component={Example} />
       </Router>
     </div>
