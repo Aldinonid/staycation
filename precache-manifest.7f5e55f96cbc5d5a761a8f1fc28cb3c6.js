af08906f283b57c3644c26a8493203779c407c53
aldinonid <aldino.efendi7@gmail.com>
2020-06-13 20:55:40 +0700

    Updates

---
+++ precache-manifest.7f5e55f96cbc5d5a761a8f1fc28cb3c6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5284b2ba28f5102eca568ff758e309",
+    "url": "/staycation/index.html"
+  },
+  {
+    "revision": "32287dc5246b89482782",
+    "url": "/staycation/static/css/2.33d22cee.chunk.css"
+  },
+  {
+    "revision": "27f00486141ea23a16cd",
+    "url": "/staycation/static/css/main.4d5cddb1.chunk.css"
+  },
+  {
+    "revision": "32287dc5246b89482782",
+    "url": "/staycation/static/js/2.cd2bc5bd.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "/staycation/static/js/2.cd2bc5bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27f00486141ea23a16cd",
+    "url": "/staycation/static/js/main.8ee06e34.chunk.js"
+  },
+  {
+    "revision": "e2d51fdff76cfbc5e5cc",
+    "url": "/staycation/static/js/runtime-main.93a4829f.js"
+  },
+  {
+    "revision": "c6ccd4740c8b875337693a033d847944",
+    "url": "/staycation/static/media/completed.c6ccd474.jpg"
+  },
+  {
+    "revision": "67e25a39beb992b5476840cc2ce5aa21",
+    "url": "/staycation/static/media/icon-calendar.67e25a39.svg"
+  },
+  {
+    "revision": "19fa22a679fb41aa197b518e0eb2d802",
+    "url": "/staycation/static/media/img-hero-frame.19fa22a6.jpg"
+  },
+  {
+    "revision": "97bfd6d983540d6604427f3bf547c0a5",
+    "url": "/staycation/static/media/img-hero.97bfd6d9.jpg"
+  },
+  {
+    "revision": "e1f52ffd6cefde4614a8547417fb990d",
+    "url": "/staycation/static/media/logo-bca.e1f52ffd.jpg"
+  },
+  {
+    "revision": "8c44d8bad4f296823c1f70bb36bfc4c6",
+    "url": "/staycation/static/media/logo-mandiri.8c44d8ba.jpg"
+  },
+  {
+    "revision": "11a62c712d17eaee33a02927afd01988",
+    "url": "/staycation/static/media/star.11a62c71.svg"
+  },
+  {
+    "revision": "1632c742f5db38b80e51f3bce1b16371",
+    "url": "/staycation/static/media/testimonial-landingpages-frame.1632c742.jpg"
+  }
+]);
